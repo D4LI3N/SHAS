dcfb354627d897887e4771e235e23a3a12cb7488
Daniel Petrovich <danielthe@cyberdude.com>
2023-09-07 21:05:03 +0200

    Update SHAS-expansion.py

--- SHAS-Expansion/SHAS-expansion.py
+++ SHAS-Expansion/SHAS-expansion.py
@@ -15,27 +15,22 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #license
-"""
-Copyright (c) 2023 Daniel Petrovich
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+license = "Copyright (c) 2023 Daniel Petrovich\n\n"
+license += "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+license += "of this software and associated documentation files (the "Software"), to deal\n"
+license += "in the Software without restriction, including without limitation the rights\n"
+license += "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+license += "copies of the Software, and to permit persons to whom the Software is\n"
+license += "furnished to do so, subject to the following conditions:\n\n"
+license += "The above copyright notice and this permission notice shall be included in all\n"
+license += "copies or substantial portions of the Software.\n\n"
+license += "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+license += "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+license += "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+license += "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+license += "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+license += "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+license += "SOFTWARE."
 
 email = ""
 epass = ""
